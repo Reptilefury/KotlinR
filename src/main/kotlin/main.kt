9dc39f0426cb6912a2f63bfe72fd500b1d2b60c4
reptilefury <reptilefury@outlook.com>
2021-06-27 20:07:21 +0100

    test

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+
+    fun printMultipleOf(multiplier: Int, andValue: Int) {
+        println("$multiplier * $andValue = ${multiplier * andValue}")
+    }
+    printMultipleOf(4,2)
+
+}
