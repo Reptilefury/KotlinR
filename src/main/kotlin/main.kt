415ff3d26ae395b956870b3568b5c38fb4a3d518
reptilefury <reptilefury@outlook.com>
2021-07-24 03:54:31 +0100

    Test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
-fun main(args: Array<String>){
-fun printMultipleOf(multplier: Int, value: Int = 1){
-    println("$multplier * $value = ${multplier * value}")
-}
-    printMultipleOf(4)
+fun main(args: Array<String>) {
+  fun add (a:Int, b:Int): Int{
+      return a + b
+  }
+    var function = ::add
+    function(4,2)
 }
