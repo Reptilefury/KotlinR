0900957ed3a016bab2c5dc3c67f11f96261a3319
reptilefury <reptilefury@outlook.com>
2021-07-27 03:42:02 +0100

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,13 @@
 fun main(args: Array<String>) {
-    var authorName: String? = null
-    var nameLength = authorName?.length
-    var nonNullableAuthor = authorName
-    var nullableInt: Int? = 10
-    var mustHaveResult = nullableInt ?: 0
-
-
-    println("Author's name has length $nameLength")
-    authorName?.let {
-        nameLength = authorName.length
+fun displaMessage(msg: String, count: Int): Int {
+    var counter = 1
+    while (counter++ <= count){
+       println(msg)
     }
-    println(mustHaveResult)
+    displaMessage("hello", 40)
+    println(msg)
+    return counter
+}
+
+
 }
