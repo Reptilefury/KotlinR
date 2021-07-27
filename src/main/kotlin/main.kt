a28cde06878bb9c91290824556778c3cff0b5ffd
reptilefury <reptilefury@outlook.com>
2021-07-27 04:12:58 +0100

    test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,24 @@
-fun main(args: Array<String>) {
-fun displaMessage(msg: String, count: Int): Int {
-    var counter = 1
-    while (counter++ <= count){
-       println(msg)
-    }
-    displaMessage("hello", 40)
-    println(msg)
-    return counter
-}
+import sun.rmi.runtime.Log
 
+fun main(args: Array<String>) {
+    fun orderProduct(
+        giftWrap: Boolean = false,
+        product: String,
+        postalService: String = "Standard"
+    ) {
+   var details: String = ""
+        if(giftWrap){
+            details += "Gift Wrapped"
+        }
+        details += "$product"
+        details += "by $postalService postage"
+        Log.i("Product details", details)
+    }
+    orderProduct(product = "Beer")
+    orderProduct(true,  "porsche")
+    orderProduct(true,  "Barbie(Jet-Set Edition)","Next Day")
+    orderProduct(product ="Flat-pack bookcase",
+    postalService = "Carrier Pigeon"
+        )
 
 }
