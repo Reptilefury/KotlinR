6ace0d96aee749f812a7babf119d0ef4c0d133c6
reptilefury <reptilefury@outlook.com>
2021-07-28 03:54:58 +0100

    Hash Map

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,40 @@
+/*
 import sun.rmi.runtime.Log
 
 fun main(args: Array<String>) {
-    fun orderProduct(
-        giftWrap: Boolean = false,
-        product: String,
-        postalService: String = "Standard"
-    ) {
-   var details: String = ""
-        if(giftWrap){
-            details += "Gift Wrapped"
+    class Person(val name: String, var weightLbs: Double) {
+        fun eatDessert(addedIcream: Boolean = true) {
+         weightLbs += if (addedIcream) 4.0 else 2.0
         }
-        details += "$product"
-        details += "by $postalService postage"
-        Log.i("Product details", details)
+        fun calcGoalWeight(weightToLoose: Double): Double {
+           return weightLbs - weightToLoose
+        }
+        val p = Person("John Doe", 170.0)
+        p.eatDessert(true)
+        val lbs = p.weightLbs
+        p.eatDessert()
+        lbs = p.weightLbs
+        val gw = p.calcGoalWeight()
+
+
     }
-    orderProduct(product = "Beer")
-    orderProduct(true,  "porsche")
-    orderProduct(true,  "Barbie(Jet-Set Edition)","Next Day")
-    orderProduct(product ="Flat-pack bookcase",
-    postalService = "Carrier Pigeon"
-        )
+}
+*/
+fun main(args:Array<String>){
+    var map = HashMap<Int, String>()
+    map.put(213,"Jenna")
+    map.put(41,"Lena")
+    map.put(32,"Seehaus")
+    map.put(46,"Farz")
+
+    println(map.get(213))
+    println(map.get(41))
+    println(map.get(32))
+    println(map.get(46))
+    for(k in map.keys){
+        println(map.get(k))
+    }
+
 
 }
+
