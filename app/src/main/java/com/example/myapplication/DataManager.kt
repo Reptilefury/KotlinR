bc14aee435be323119c5d6725b33d3aaaca1439f
reptilefury <reptilefury@outlook.com>
2021-08-05 16:24:45 +0100

    Note Keeper Data

---
+++ app/src/main/java/com/example/myapplication/DataManager.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+class DataManager {
+    var courses = HashMap<String, CourseInfo>()
+    var notes = ArrayList<NoteInfo>()
+
+}
---
+++ app/src/main/java/com/example/myapplication/NoteKeeperData.kt
@@ -0,0 +1,4 @@
+package com.example.myapplication
+
+class CourseInfo (val courseId: String,val title: String)
+class NoteInfo( var course: CourseInfo, var title: String, var text: String)
